f45915f8ddfea5b76848fcca9e3f1d10deef8ca4
luc.perroquin <luc.perroquin@gmail.com>
2026-08-24 10:23:20 +0200

    Add CostLab service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='costlab-v3';
+const ASSETS=['./', './index.html', './styles.css', './manifest.webmanifest', './js/core.js', './js/render-a.js', './js/render-b1.js', './js/render-b2.js', './js/schemas.js', './js/items.js', './js/ui.js', './js/windows.js', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-512-maskable.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const clone=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return resp}).catch(()=>caches.match('./index.html')))));
